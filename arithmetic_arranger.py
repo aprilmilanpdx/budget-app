5cff9dda954d32056cb3f10245ddfbb7ba7ab481
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-01-18 15:37:03 -0800

    created solutions list of values list to prepare to iterate through solutions to create line output

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -6,6 +6,9 @@ def arithmetic_arranger(problems, solve = False):
   # error if more than 5 problems
   if len(problems) > 5:
     return "Error: Too many problems."
+  
+  # create empty list of solutions
+  solutions = []
 
   # error if anything other than digits entered in numbers and if operator is not "+" or "-"
   for problem in problems:
@@ -24,13 +27,13 @@ def arithmetic_arranger(problems, solve = False):
     if len(num1) > 4 or len(num2) > 4:
       return "Error: Numbers cannot be more than four digits."
     
-    # calculate longest line length and append output as "width" to values list
+    # calculate longest line length 
+    line = ""
     width = max(len(num1), len(num2)) + 2
-    values.append(width)
-    
-    #print list of list to check progress
-    print(values)
-
+    for i in range(width):
+      line += "-"
+    values.append(line)
+        
     #perform problem and append answer to values list
     if operator == "+":
       answer = str(int(num1) + int(num2))
@@ -38,14 +41,25 @@ def arithmetic_arranger(problems, solve = False):
       answer = str(int(num1) - int(num2))
     values.append(answer)
 
-  # create output lines
-    line1 = num1.rjust(width)
-    line2 = operator + num2.rjust(width - 1)
-    line3 = "-" * (width)
-    line4 = str(answer).rjust(width)
+    solutions.append(values)
   
-    solution = line1 + '    \n' + line2 + '    \n' + line3 
-    print(solution)
+
+    # create output lines
+    # line1 = ""
+    # line2 = ""
+    # line3 = ""
+    # line4 = ""
+
+    # iterate through ********* to create joined data for lines 
+    # for output in values:
+
+    # line1 = output[0].rjust(width) + " " * 4
+    # line2 = output[1] + output[2].rjust(width - 1) + " " * 4
+    # line3 = output[3] + " " * 4
+    # line4 = output[4].rjust(width) + " " * 4
+  
+    # solution = line1 + '    \n' + line2 + '    \n' + line3 
+    # print(solution)
 
 arithmetic_arranger(problemset)
 
