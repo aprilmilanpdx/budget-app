53e66e32ca28c95c0306d5dba9ad8cf7a5a42aa4
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-01-18 15:16:42 -0800

    added coments, orgnaized order of methods within arithmetic arranger function

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -3,45 +3,49 @@ problemset = ['32 + 698', '3801 - 2', '45 + 43', '123 + 49']
 import re
 
 def arithmetic_arranger(problems, solve = False):
+  # error if more than 5 problems
   if len(problems) > 5:
     return "Error: Too many problems."
 
-  num1 = ""
-  num2 = ""
-  operator = ""
-  answer = ""
-
- # split problems by space and create num1, num2, and operator variables
+  # error if anything other than digits entered in numbers and if operator is not "+" or "-"
   for problem in problems:
     if re.search("[^\s0-9.+-]", problem):
         if re.search("[*]", problem) or re.search("[/]", problem):
           return "Error: Operator must be '+' or '-'."
         return "Error: Numbers must only contain digits."
 
-    num1 = problem.split()[0]
-    num2 = problem.split()[2]
-    operator = problem.split()[1]
+    # split problems by space and create num1, num2, and operator variables
+    values = problem.split()
+    num1 = values[0]
+    num2 = values[2]
+    operator = values[1]
     
+    # error if any number is more than 4 digits
     if len(num1) > 4 or len(num2) > 4:
       return "Error: Numbers cannot be more than four digits."
     
-    solution = ""
-    if operator == "+":
-      solution = str(int(num1) + int(num2))
-    else:
-      solution = str(int(num1) - int(num2))
-
-# calculate longest line length
-    width = max(len(num1), len(num2))
+    # calculate longest line length and append output as "width" to values list
+    width = max(len(num1), len(num2)) + 2
+    values.append(width)
     
+    #print list of list to check progress
+    print(values)
+
+    #perform problem and append answer to values list
+    if operator == "+":
+      answer = str(int(num1) + int(num2))
+    else:
+      answer = str(int(num1) - int(num2))
+    values.append(answer)
+
   # create output lines
-    line1 = num1.rjust(width + 2)
-    line2 = operator + num2.rjust(width + 1)
-    line3 = "-" * (width + 2)
-    line4 = str(solution).rjust(width + 2)
+    line1 = num1.rjust(width)
+    line2 = operator + num2.rjust(width - 1)
+    line3 = "-" * (width)
+    line4 = str(answer).rjust(width)
   
-    answer = line1 + '    \n' + line2 + '    \n' + line3 
-    print(answer)
+    solution = line1 + '    \n' + line2 + '    \n' + line3 
+    print(solution)
 
 arithmetic_arranger(problemset)
 
