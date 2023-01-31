46be49fc601f6c13e82a5e65f04c97c917de1174
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-01-31 14:28:14 -0800

    starting code

---
+++ budget.py
@@ -0,0 +1,12 @@
+class Category:
+  # def __init__(self) -> None:
+    # pass
+    def __init__(self, category, balance):
+      self.category = category
+      self.balance = balance
+
+
+
+
+
+# def create_spend_chart(categories):
