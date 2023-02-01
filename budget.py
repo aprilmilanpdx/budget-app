b7bda42154d11fa4674d33ba9e5a12943f018ff3
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-01-31 17:30:25 -0800

    working class and output prior to coding create_spend_chart

--- budget.py
+++ budget.py
@@ -6,11 +6,11 @@ class Category:
       self.ledger = []
     
     def __str__(self):
-      heading = f"{self.name}.center(30,*)\n"
+      heading = f"{self.name}".center(30,'*') + "\n"
       items = ""
       total = 0
       for item in self.ledger:
-        items += f"{item['description'][:23]}" + f"({item['amount']:>7}" + "\n"
+        items += f"{item['description'][:23]:23}" + f"{item['amount']:>7.2f}" + "\n"
         total += item['amount']
       output = heading + items + "Total: " + str(total)
       return output
@@ -19,19 +19,25 @@ class Category:
       """accepts an amount and description(default: empty string).  Appends object to the ledger list in the form of {"amount": amount, "description": description}"""
       self.ledger.append({"amount": amount, "description": description})
 
-    def withdrawal(self, amount, description = ""):
-      """accepts an amount and description(default: empty string). amount stored in the ledger as negative. If there are not enough funds, nothing should be added to the ledger. This method should return True if the withdrawal took place, and False otherwise"""
-      if self.check_funds(amount):
-        self.ledger.append({"amount": -amount, "description": description})
-        return True
-      return False
-
     def get_balance(self):
       """returns the current balance of the budget category based on the deposits and withdrawals that have occurred"""
       balance = 0
       for entry in self.ledger:
         balance += entry["amount"]
       return balance
+
+    def check_funds(self, amount):
+      """Accepts an amount as an argument. It returns False if the amount is greater than the balance of the budget category and returns True otherwise. This method should be used by both the withdraw method and transfer method."""
+      if self.get_balance() > amount:
+        return True
+      return False
+
+    def withdraw(self, amount, description = ""):
+      """accepts an amount and description(default: empty string). amount stored in the ledger as negative. If there are not enough funds, nothing should be added to the ledger. This method should return True if the withdrawal took place, and False otherwise"""
+      if self.check_funds(amount):
+        self.ledger.append({"amount": -amount, "description": description})
+        return True
+      return False
     
     def transfer(self, amount, category):
       """Accepts an amount and another budget category as arguments. Adds a withdrawal with the amount and the description "Transfer to [Destination Budget Category]". The method then adds a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]". If there are not enough funds, nothing should be added to either ledgers. This method should return True if the transfer took place, and False otherwise."""
@@ -41,21 +47,7 @@ class Category:
         return True
       return False
 
-    def check_funds(self, amount):
-      """Accepts an amount as an argument. It returns False if the amount is greater than the balance of the budget category and returns True otherwise. This method should be used by both the withdraw method and transfer method."""
-      if self.get_balance > amount:
-        return False
-      return True
-
-
-
-
-
     
-
-
-
-
 
 
 # def create_spend_chart(categories):
